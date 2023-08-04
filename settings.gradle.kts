68a93c0633dbc49a854efed9c04e388085a3d752
Daffa Muhtar <daffa.muhtar@gmail.com>
2023-08-04 14:36:11 +0700

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Gotta_Proved"
+include(":Gotta_Prove_App")
+include(":sharedGPA")
---
+++ sharedGPA/src/androidMain/kotlin/com/daffamuhtar/gottaproved/Platform.kt
@@ -0,0 +1,7 @@
+package com.daffamuhtar.gottaproved
+
+class AndroidPlatform : Platform {
+    override val name: String = "Android ${android.os.Build.VERSION.SDK_INT}"
+}
+
+actual fun getPlatform(): Platform = AndroidPlatform()
